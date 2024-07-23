2391ad6bc085109a61953db1f0cd2283fa367633
lsy <lesiyu129@163.com>
2024-07-23 20:42:16 +0800

    feat: tf

---
+++ day1/p1.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+t1 = tf.constant(4)
+t2 = tf.constant([1,2,3])
+t3 = tf.constant([[1],[2],[3]])
+print('1', t1)
+print('2', t1.shape)
+print('1', t2)
+print('2', t2.shape)
+print('1', t3)
+print('2', t3.shape)
+
